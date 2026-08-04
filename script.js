8c8d10ced978eedaa0df112118551018cb3181d2
bengkhng645-droid <bengkhng645@gmail.com>
2026-08-04 13:07:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
-document. addEventListener(DOMContentLoaded',function () {
-    console. log('Portfolio ของ bengkhng645 พร้อมแล้ว!');
-});
+// เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใล้ (Optional)
+document.addEventListener('DOMContentLoaded',function () {
+   console.log('Portfolio ของ bengkhng พร้อมแล้ว!');
+}};
