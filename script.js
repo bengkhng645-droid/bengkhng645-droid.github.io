38ba3da83a9879fc228ab7039322a25d86358edd
bengkhng645-droid <bengkhng645@gmail.com>
2026-08-04 12:33:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
 document. addEventListener(DOMContentLoaded',function () {
-    console. 1og('Portfolio ของ ธนุตน์ พร้อมแล้ว!');
+    console. 1og('Portfolio ของ thanut พร้อมแล้ว!');
 });
