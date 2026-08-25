a9ffb45873940b28be29e496f9eb6809705b7b54
bengkhng645-droid <bengkhng645@gmail.com>
2026-08-25 09:05:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใล้ (Optional)
 document.addEventListener('DOMContentLoaded',function () {
+   document.getElementById('year').innerHML = new Date().getFu11Year();
    console.log('Portfolio ของ ธนุตน์ พร้อมแล้ว!');
 }};
